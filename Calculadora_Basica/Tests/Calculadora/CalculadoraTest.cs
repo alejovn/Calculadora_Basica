c19a063808b92e52afcedb9f2f453fd71c38c1cb
alejandro.vnavarro <alejandro.vnavarro@AUTOP-1LHU0WWQ2>
2024-11-20 18:36:57 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora_Basica/Tests/Calculadora/CalculadoraTest.cs
@@ -0,0 +1,16 @@
+namespace Calculadora_Basica.Tests.Calculadora
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
